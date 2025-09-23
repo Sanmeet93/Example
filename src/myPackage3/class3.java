064ba7f2182292876e73cbbfaab8831de4d7970f
Sanmeetkumar <Sanmeetkumar@DESKTOP-3V0K8EB>
2025-09-23 21:06:37 +0530

    learning pull request

--- src/myPackage3/class3.java
+++ src/myPackage3/class3.java
@@ -4,6 +4,8 @@ public class class3 {
 
 	public static void main(String[] args) {
 		System.out.println("hey world");
+		System.out.println("hey world");
+		System.out.println("hey world");
 	}
 	
 }
